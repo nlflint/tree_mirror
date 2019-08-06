76251e80335ed947db51ff09f98e80263a20ac2d
Nathan Flint <nlflint@gmail.com>
2019-08-05 22:15:43 -0700

    Simple case implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::mem;
+
+#[derive(PartialEq, Debug)]
+struct Node<T> {
+    value: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+}
+
+fn mirror_tree<T>(mut node: Node<T>) -> Node<T> {
+    mem::swap(&mut node.left, &mut node.right);
+    return node;
+}
+
+//    10
+//   /  \
+// <>    11
+#[test]
+fn mirror_two_deep_tree() {
+    let root = Node::<usize> {
+        value: 10,
+        left: Option::None,
+        right: Option::Some(
+            Box::new(
+                Node::<usize> {
+                    value: 11,
+                    left: Option::None,
+                    right: Option::None
+                }
+            )   
+        )
+    };
+
+    let mirrored = mirror_tree::<usize>(root);
+
+    assert!(mirrored == 
+        Node::<usize> {
+            value: 10,
+            right: Option::None,
+            left: Option::Some(
+                Box::new(
+                    Node::<usize> {
+                        value: 11,
+                        left: Option::None,
+                        right: Option::None
+                    }
+                )   
+            )
+        }
+    )
 }

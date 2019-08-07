59ca6de1b780eac8bf37e7230683363edf0a355e
Nathan Flint <nlflint@gmail.com>
2019-08-06 22:12:38 -0700

    Full implementation complete

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,27 @@ struct Node<T> {
     right: Option<Box<Node<T>>>,
 }
 
-fn mirror_tree<T>(mut node: Node<T>) -> Node<T> {
-    mem::swap(&mut node.left, &mut node.right);
-    return node;
+fn mirror_tree<T>(mut root: Node<T>) -> Node<T> {
+    let mut _nodes = Vec::<&mut Node<T>>::new();
+    
+    _nodes.push(&mut root);
+
+    while let Some(node) = _nodes.pop() {
+
+        mem::swap(&mut node.left, &mut node.right);
+
+        match node.left {
+            Some(ref mut boxed) => _nodes.push(boxed),
+            _ => ()
+        }
+
+        match node.right {
+            Some(ref mut boxed) => _nodes.push(boxed),
+            _ => ()
+        }
+    }
+
+    return root;
 }
 
 //    10
@@ -48,4 +66,87 @@ fn mirror_two_deep_tree() {
             )
         }
     )
+}
+
+#[test]
+fn mirror_three_deep_tree() {
+    let root = Node::<usize> {
+        value: 1,
+        right: Option::Some(
+            Box::new(
+                Node::<usize> {
+                    value: 5,
+                    left: Option::None,
+                    right: Option::None
+                }
+            )
+        ),
+        left: Option::Some(
+            Box::new(
+                Node::<usize> {
+                    value: 2,
+                    left: Option::Some(
+                        Box::new(
+                            Node::<usize> {
+                                value: 11,
+                                left: Option::None,
+                                right: Option::None
+                            }
+                        )
+                    ),
+                    right: Option::Some(
+                        Box::new(
+                            Node::<usize> {
+                                value: 12,
+                                left: Option::None,
+                                right: Option::None
+                            }
+                        )
+                    )
+                }
+            )   
+        )
+    };
+
+    let mirrored = mirror_tree::<usize>(root);
+
+    assert!(mirrored == 
+        Node::<usize> {
+            value: 1,
+            right: Option::Some(
+                Box::new(
+                    Node::<usize> {
+                        value: 2,
+                        right: Option::Some(
+                            Box::new(
+                                Node::<usize> {
+                                    value: 11,
+                                    left: Option::None,
+                                    right: Option::None
+                                }
+                            )
+                        ),
+                        left: Option::Some(
+                            Box::new(
+                                Node::<usize> {
+                                    value: 12,
+                                    left: Option::None,
+                                    right: Option::None
+                                }
+                            )
+                        )
+                    }
+                )   
+            ),
+            left: Option::Some(
+                Box::new(
+                    Node::<usize> {
+                        value: 5,
+                        left: Option::None,
+                        right: Option::None
+                    }
+                )
+            ),
+        }
+    );
 }
